80cd63fd914d1eb02e151ef567592e05f4568bae
mgrcar <miha@sowalabs.com>
2011-04-20 12:35:50 +0000

    BoilerplateRemover now uses HtmlTokenizerHap. Several changes to HtmlTokenizer. Anyhow, this class is obsolete / replaced with HtmlTokenizerHap. Added HtmlTokenizerHap.cs. Added BoilerplateRemoverComponent.cs.
    git-svn-id: svn://first.ijs.si:3691/latino/LatinoWorkflows@192 35089d11-96e9-ae4d-82a0-4b2588c93174

---
+++ TextMining/BoilerplateRemoverComponent.cs
@@ -0,0 +1,58 @@
+/*==========================================================================;
+ *
+ *  This file is part of LATINO. See http://latino.sf.net
+ *
+ *  File:    BoilerplateRemoverComponent.cs
+ *  Desc:    Boilerplate remover component 
+ *  Created: Apr-2011
+ *
+ *  Authors: Miha Grcar
+ *
+ ***************************************************************************/
+
+using System;
+using System.IO;
+using System.Text;
+using Latino.TextMining;
+
+namespace Latino.Workflows.TextMining
+{
+    /* .-----------------------------------------------------------------------
+       |
+       |  Class BoilerplateRemoverComponent
+       |
+       '-----------------------------------------------------------------------
+    */
+    public class BoilerplateRemoverComponent : DocumentProcessor
+    {
+        public BoilerplateRemoverComponent() : base(typeof(BoilerplateRemoverComponent).ToString())
+        {
+        }
+
+        protected override void ProcessDocument(Document document)
+        {
+            string contentType = document.Features.GetFeatureValue("_contentType");
+            if (contentType != null && contentType != "Html") { return; } // TODO: handle XHTML as well
+            try
+            {
+                HtmlTokenizerHap tok = new HtmlTokenizerHap(document.Text);
+                tok.Normalize = false;
+                int numTags = 0;
+                foreach (string token in tok)
+                {
+                    Console.WriteLine(token);
+                    numTags++;
+                }
+                mLogger.Info("ProcessDocument", "************************************************************* {0}", numTags);
+                //document.Features.SetFeatureValue("_contentType", "Text");
+            }
+            catch (Exception e)
+            {
+                Guid docGuid = Guid.NewGuid();
+                mLogger.Error("ProcessDocument", "hapfail {0}", docGuid);
+                File.WriteAllText(@"c:\ex\" + docGuid + ".html", document.Text);
+                mLogger.Error("ProcessDocument", e);
+            }
+        }
+    }
+}
