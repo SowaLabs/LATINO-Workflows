160119ac4053471562461bfd10e9250ac243e1ad
mgrcar <miha@sowalabs.com>
2014-02-15 22:53:57 +0100

    HtmlTokenizerComponent: added additional skip tags (forms, embedded content, header, footer, nav, aside...)

--- TextMining/HtmlTokenizerComponent.cs
+++ TextMining/HtmlTokenizerComponent.cs
@@ -27,21 +27,18 @@ namespace Latino.Workflows.WebMining
     */
     public class HtmlTokenizerComponent : DocumentProcessor
     {
-        private static Set<string> mTagSkipList = new Set<string>("ins,del,bdo,em,strong,dfn,code,samp,kbd,var,cite,abbr,acronym,q,sub,sup,tt,i,b,big,small,u,s,strike,basefont,font,a".Split(','));
-        private static Set<string> mTagKeepList = Set<string>.Difference(
-            // the following list of HTML5 tags is taken from http://www.quackit.com/html_5/tags/
-            new Set<string>("!doctype,a,abbr,address,area,article,aside,audio,b,base,bb,bdo,blockquote,body,br,button,canvas,caption,cite,code,col,colgroup,command,datagrid,datalist,dd,del,details,dfn,div,dl,dt,em,embed,eventsource,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,head,header,hgroup,hr,html,i,iframe,img,input,ins,kbd,keygen,label,legend,li,link,mark,map,menu,meta,meter,nav,noscript,object,ol,optgroup,option,output,p,param,pre,progress,q,ruby,rp,rt,samp,script,section,select,small,source,span,strong,style,sub,summary,sup,table,tbody,td,textarea,tfoot,th,thead,time,title,tr,ul,var,video,wbr".Split(',')),
-            mTagSkipList);
+        private static Set<string> mTagSkipList 
+            = new Set<string>("ins,del,bdo,em,strong,dfn,code,samp,kbd,var,cite,abbr,acronym,q,sub,sup,tt,i,b,big,small,u,s,strike,basefont,font,a".Split(','));
+        private static Set<string> mTagKeepList 
+            = Set<string>.Difference(
+                // the following list of HTML5 tags is taken from http://www.quackit.com/html_5/tags/
+                new Set<string>("!doctype,a,abbr,address,area,article,aside,audio,b,base,bb,bdo,blockquote,body,br,button,canvas,caption,cite,code,col,colgroup,command,datagrid,datalist,dd,del,details,dfn,div,dl,dt,em,embed,eventsource,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,head,header,hgroup,hr,html,i,iframe,img,input,ins,kbd,keygen,label,legend,li,link,mark,map,menu,meta,meter,nav,noscript,object,ol,optgroup,option,output,p,param,pre,progress,q,ruby,rp,rt,samp,script,section,select,small,source,span,strong,style,sub,summary,sup,table,tbody,td,textarea,tfoot,th,thead,time,title,tr,ul,var,video,wbr".Split(',')), 
+                mTagSkipList);
         private Set<string> mAdditionalSkipTags
-            = new Set<string>();
+            = new Set<string>("form,fieldset,legend,label,input,button,select,datalist,optgroup,option,textarea,keygen,output,progress,meter,details,summary,menuitem,menu,img,iframe,embed,object,param,video,audio,source,track,canvas,map,area,svg,math,nav,aside,header,footer,address".Split(','));
 
         public HtmlTokenizerComponent() : base(typeof(HtmlTokenizerComponent))
         {
-        }
-
-        public void SetAdditionalSkipTags(IEnumerable<string> skipTags)
-        {
-            mAdditionalSkipTags = new Set<string>(skipTags);
         }
 
         public/*protected*/ override void ProcessDocument(Document document)
